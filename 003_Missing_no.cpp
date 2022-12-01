8abe586af896432f195d3aeb9a8e3df12d700de2
Sankalp Borse <102895798+Sankalp-borse@users.noreply.github.com>
2022-12-01 23:59:02 +0530

    Create 003_Missing_no.cpp

---
+++ 003_Missing_no.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution{
+  public:
+    int MissingNumber(vector<int>& array, int n) {
+        int a = (n) * (n + 1) / 2;
+        int b=0;
+            for (int i = 0; i < n-1; i++){
+                b = b+array[i];
+            }
+        return a-b;
+    };
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> array(n - 1);
+        for (int i = 0; i < n - 1; ++i) cin >> array[i];
+        Solution obj;
+        cout << obj.MissingNumber(array, n) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
